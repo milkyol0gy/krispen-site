9c586b6d22157d5cf66f7d80ea0eaccf27985074
milkyol0gy <valenciaoctavianyerica@gmail.com>
2025-10-08 22:07:33 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
     });
 
     // Route names will now be materials.index, materials.create, etc. (NO 'admin.' prefix)
-    Route::get('/materials', [MaterialController::class, 'index'])->name('materials.index');
-    Route::get('/materials/create', [MaterialController::class, 'create'])->name('materials.create');
-    Route::post('/materials/insert', [MaterialController::class, 'store'])->name('materials.insert');
-    Route::get('/materials/edit/{material}', [MaterialController::class, 'edit'])->name('materials.edit');
-    Route::put('/materials/update/{material}', [MaterialController::class, 'update'])->name('materials.update');
-    Route::delete('/materials/destroy/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
+    Route::prefix('materials')->name('materials.')->group(function () {
+        Route::get('/', [MaterialController::class, 'index'])->name('materials.index');
+        Route::get('/materials/create', [MaterialController::class, 'create'])->name('materials.create');
+        Route::post('/materials/insert', [MaterialController::class, 'store'])->name('materials.insert');
+        Route::get('/materials/edit/{material}', [MaterialController::class, 'edit'])->name('materials.edit');
+        Route::put('/materials/update/{material}', [MaterialController::class, 'update'])->name('materials.update');
+        Route::delete('/materials/destroy/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
+    });
+    
 
 
 
